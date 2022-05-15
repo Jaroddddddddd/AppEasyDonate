e2915a659953a49f397d79d8438e7fdaed4c5e4c
Jaroddddddddd <e1315332336@live.uleam.edu.ec>
2022-05-14 19:52:45 -0500

    pages

---
+++ app_easy_donate/lib/utilities/MyRoutes.dart
@@ -0,0 +1,8 @@
+class MyRoutes {
+  static String homeRoute = '/home';
+  static String loginRoute = '/login';
+  static String donateRoute = '/donate';
+  static String submitRoute = '/submit';
+  static String helpUsRoute = '/helpUs';
+  static String JoinUsRoute = '/JoinUs';
+}
